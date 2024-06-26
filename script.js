3db932590df46821613b93ddf484d55a30c65afe
Wojciech Kogut <wojtek.kogut@o2.pl>
2024-06-26 16:06:08 +0200

    update cv

--- script.js
+++
@@ -1,9 +0,0 @@
-const el = document.querySelector('main');
-
-document.querySelector('.circle').addEventListener('click', (el) => {
-    scrollBy({
-        behavior: "smooth",
-        left: 0,
-        top: window.innerHeight,
-    })
-});
